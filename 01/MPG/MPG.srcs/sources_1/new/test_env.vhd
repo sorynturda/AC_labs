b5cde4b26b3b52b896f80dff7423afe15aabe8bf
Sorin Turda <s.turda12@gmail.com>
2024-03-06 07:51:20 +0200

    mersi dan

--- 01/MPG/MPG.srcs/sources_1/new/test_env.vhd
+++ 01/MPG/MPG.srcs/sources_1/new/test_env.vhd
@@ -13,11 +13,63 @@ end test_env;
 
 architecture Behavioral of test_env is
 
+component MPG is
+    Port (
+        clk: in STD_LOGIC;
+        btn: in STD_LOGIC;
+        enable: out STD_LOGIC);
+end component;
+
+signal cnt: std_logic_vector (2 downto 0) := "000";
+signal en: std_logic := '0';
+signal dir: std_logic := '0';
+signal dcd_out: std_logic_vector (7 downto 0) := x"00";
+
+
 begin
 
-led <= sw;
-an(7 downto 0) <= "11111111";
-an(3 downto 0) <= btn(3 downto 0);
-cat <= (others => '0');
+--led <= sw;
+--an(7 downto 0) <= "11111111";
+--an(3 downto 0) <= btn(3 downto 0);
+--cat <= (others => '0');
+
+
+mapMPG: MPG port map(clk,btn(0),en);
+dir <= sw(0);
+--counter
+ process(clk)
+   begin
+        if rising_edge(clk) then 
+           if en = '1' then
+               if dir = '0' then
+                   cnt <= cnt + 1;
+               else
+                   cnt <= cnt - 1;
+               end if;
+           end if;
+       end if;
+   end process;
+--end counter
+
+--dcd 3:8
+process(cnt)
+begin
+    case cnt is
+        when "000" => dcd_out <= "00000001";
+        when "001" => dcd_out <= "00000010";
+        when "010" => dcd_out <= "00000100";
+        when "011" => dcd_out <= "00001000";
+        when "100" => dcd_out <= "00010000";
+        when "101" => dcd_out <= "00100000";
+        when "110" => dcd_out <= "01000000";
+        when "111" => dcd_out <= "10000000";
+        when others => dcd_out <= x"00";
+    end case;    
+end process;
+--end dcd
+
+led(7 downto 0) <= dcd_out;
+led(12 downto 8) <= "00000";
+led(15 downto 13) <= cnt;
 
 end Behavioral;
